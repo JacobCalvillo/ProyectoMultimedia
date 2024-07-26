8fb00224f03c55a732c9e9a8e78b1c0b2a08b011
Jacob Calvillo <jakeob99@gmail.com>
2024-07-26 17:14:22 -0600

    fix config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  
+  base: 'https://JacobCalvillo.github.io/ProyectoMultimedia/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, './src/earth.html')
+      }
+    }
+  }
 });
